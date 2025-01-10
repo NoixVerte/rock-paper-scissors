e253a087bb0a773975d9fee910dc6738d74d1b3d
NoixVerte <65561735+NoixVerte@users.noreply.github.com>
2025-01-10 15:03:01 +0200

    Make basic UI and show the score in plain text

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let humanScore = 0;
 let computerScore = 0;
+const humanScoreText = document.querySelector("#humanScore");
+const computerScoreText = document.querySelector("#computersScore");
+const btnRock = document.querySelector("#Rock");
+const btnPaper = document.querySelector("#Paper");
+const btnScissors = document.querySelector("#Scissors");
+const buttons = document.querySelectorAll("button");
 const choices = ["Rock", "Paper", "Scissors"];
 const outcomes = [[0, -1, 1], [1, 0, -1], [-1, 1, 0]];
 
@@ -17,15 +23,6 @@ function getComputerChoice() {
     } 
 }
 
-function getHumanChoice() {
-    let choice;
-    do {
-        choice = (prompt("Choose Rock, Paper, or Scissors!"));
-        choice = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase()
-    } while (choice != "Rock" && choice != "Paper" && choice != "Scissors");
-    return choice;
-}
-
 function playRound(humanChoice, computerChoice) {
     console.log(`You chose ${humanChoice}, computer chose ${computerChoice}`);
     switch(outcomes[choices.indexOf(humanChoice)][choices.indexOf(computerChoice)])  {
@@ -41,19 +38,28 @@ function playRound(humanChoice, computerChoice) {
             humanScore++;
             break;
     }
+    humanScoreText.textContent = humanScore.toString();
+    computerScoreText.textContent = computerScore.toString();
 }
 
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        console.log(`-----Round ${i + 1}!-----`);
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-    console.log(`Final score:
-    Your score: ${humanScore}
-    Computer's score: ${computerScore}`);
-    if (humanScore > computerScore) console.log("Congratilations! You win!");
-    else if (humanScore < computerScore) console.log("Unfortunately, you lost.");
-    else console.log("Anticlimactic! It's a tie.");
-}
+buttons.forEach(button => {
+    let humanChoice = button.id;
+    button.addEventListener("click", () => {
+        playRound(humanChoice, getComputerChoice());
+    })
+});
 
-playGame();
+// function playGame() {
+//     for (let i = 0; i < 5; i++) {
+//         console.log(`-----Round ${i + 1}!-----`);
+//         playRound(getHumanChoice(), getComputerChoice());
+//     }
+//     console.log(`Final score:
+//     Your score: ${humanScore}
+//     Computer's score: ${computerScore}`);
+//     if (humanScore > computerScore) console.log("Congratilations! You win!");
+//     else if (humanScore < computerScore) console.log("Unfortunately, you lost.");
+//     else console.log("Anticlimactic! It's a tie.");
+// }
+
+// playGame();
